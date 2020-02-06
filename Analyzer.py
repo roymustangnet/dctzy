2672400e2fe4a52683a100cec54a0bbc684efe22
supakito <roy.mustang.net@gmail.com>
2020-02-06 20:56:18 +0800

    git init

---
+++ Analyzer.py
@@ -0,0 +1,139 @@
+import os
+from collections import Counter
+import pandas as pd
+import jieba
+import jieba.posseg
+import jieba.analyse
+import utils
+
+
+'''
+停用词表来源：
+https://github.com/goto456/stopwords
+'''
+class OutpatientVolAnalyzer:
+    '''
+    从DataFrame的特定字段中
+    统计所有词项的频率
+    '''
+    def __init__(self,
+                 data:pd.DataFrame,
+                 focused_fields:list=['DIAG_DESC'],
+                 user_dict:str='./mydicts.txt',
+                 stopwords_file:str ='./stopwords.txt'):
+        '''
+        :param data: 需要处理的门诊数据
+        :param focused_fields: 需要处理的字段
+        :param user_dict: 用户自定义的字典
+        :param stopwords_file: 停用词文件
+        '''
+        self._data = data
+        self._focused_fields = focused_fields
+        self._stopwords = self.__get_stopwords(stopwords_file)
+        self._c = Counter()
+        jieba.load_userdict(user_dict)
+        # jieba.analyse.load_userdict(user_dict)
+
+
+    def get_freq_rank(self, poses:list=['n', 'a'], topK:int=20, clear=False):
+        '''
+        获得词频最高的词项
+        :param topK: 前n项的数量
+        :param poses: 词性，可选择的包括
+        n	普通名词	f	方位名词	s	处所名词	t	时间
+        nr	人名	ns	地名	nt	机构名	nw	作品名
+        nz	其他专名	v	普通动词	vd	动副词	vn	名动词
+        a	形容词	ad	副形词	an	名形词	d	副词
+        m	数量词	q	量词	r	代词	p	介词
+        c	连词	u	助词	xc	其他虚词	w	标点符号
+        PER	人名	LOC	地名	ORG	机构名	TIME	时间
+        :return: 词频排名靠前的前N个词项
+        '''
+        if clear or len(self._c) == 0:
+            self._c.clear()
+            for field in self._focused_fields:
+                for row in self._data[field]:
+                    self.__get_words(row, poses)
+        return self._c.most_common(topK)
+
+    def get_tf_idf_rank(self, poses:list=['n', 'a'], topK:int=20):
+        '''
+        获得TF-IDF的排名靠前的词项
+        :param topK: 前n项的数量
+        :param poses: 词性，可选择的包括
+        n	普通名词	f	方位名词	s	处所名词	t	时间
+        nr	人名	ns	地名	nt	机构名	nw	作品名
+        nz	其他专名	v	普通动词	vd	动副词	vn	名动词
+        a	形容词	ad	副形词	an	名形词	d	副词
+        m	数量词	q	量词	r	代词	p	介词
+        c	连词	u	助词	xc	其他虚词	w	标点符号
+        PER	人名	LOC	地名	ORG	机构名	TIME	时间
+        :return: TF-IDF排名靠前的前N个词项
+        '''
+        sentence = self.__get_sentence()
+        return jieba.analyse.extract_tags(sentence, topK=topK, withWeight=True, allowPOS=poses)
+
+
+    def get_textrank_rank(self, poses:list=['n', 'a'], topK=20):
+        '''
+        获得TextRank最高的词项
+        :param topK: 前n项的数量
+        :param poses: 词性，可选择的包括
+        n	普通名词	f	方位名词	s	处所名词	t	时间
+        nr	人名	ns	地名	nt	机构名	nw	作品名
+        nz	其他专名	v	普通动词	vd	动副词	vn	名动词
+        a	形容词	ad	副形词	an	名形词	d	副词
+        m	数量词	q	量词	r	代词	p	介词
+        c	连词	u	助词	xc	其他虚词	w	标点符号
+        PER	人名	LOC	地名	ORG	机构名	TIME	时间
+        :return: TextRank排名靠前的前N个词项
+        '''
+        sentence = self.__get_sentence()
+        return jieba.analyse.textrank(sentence, topK=topK, withWeight=True, allowPOS=poses)
+
+    def __get_words(self, txt, poses:list=['n', 'a']):
+        '''
+        对文档进行分词
+        :param txt: 输入的文本
+        :param poses: 需要保留的词性
+        :return:
+        '''
+        words = jieba.posseg.cut(txt)
+        for w in words:
+            if w.word not in self._stopwords and (not poses or w.flag in poses):
+                self._c[w.word] += 1
+
+    def __get_stopwords(self, stopwords_file):
+        '''
+        获得停用词字典
+        同时设置jieba分析器的停用词表
+        :param stopwords_file:停用词表的文件
+        :return:停用词字典
+        '''
+        if not(stopwords_file and os.path.exists(stopwords_file)):
+            folder = './stopwords'
+            stopwords_file = './stopwords.txt'
+            utils.merage_file('./stopwords', stopwords_file)
+            # 设置jieba的停用词
+            jieba.analyse.set_stop_words(stopwords_file)
+        return (line.strip() for line in open(stopwords_file, encoding='utf-8').readlines())
+
+    def __get_sentence(self):
+        '''
+        将所有关心的字段中的文字合并在一起
+        :return:
+        '''
+        sentence = ''
+        for field in self._focused_fields:
+            for row in self._data[field]:
+                try:
+                    if not row.endswith('.|。|，|,'):
+                        row += '。'
+                except Exception as e:
+                    print(row)
+                sentence += row
+        return sentence
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,128 @@
+# -*- coding: utf-8 -*-
+import os
+import pandas as pd
+import jieba
+
+
+REMOVED_WORDS = ['复查', '复诊', '取药', '开药', 'fuchsa', 'fuccha', '挂错号', '买药']
+PAT = r'^(?:(?!代诊).)*(' + '|'.join(REMOVED_WORDS) + r')(?:(?!代诊).)*$'
+
+def batch_read(cols:list=['PATIENT_ID', 'VISIT_DATE', 'SEX', 'DIAG_DESC', 'ILLNESS_DESC'],
+               is_filter:bool = True,
+               filter_cols:list = ['ILLNESS_DESC'],
+               filter_exps:list = [PAT]):
+    '''
+    批量读取数据
+    :param cols: 需要读取的字段
+    :param is_filter: 是否过滤数据，默认过滤
+    :return:
+    '''
+    data_1 = read('./data/2016年心身科门诊数据.csv', cols = cols, is_filter= is_filter,
+                  filter_cols = filter_cols, filter_exps = filter_exps)
+    data_2 = read('./data/2017年心身科门诊数据.csv', cols = cols, is_filter= is_filter,
+                  filter_cols = filter_cols, filter_exps = filter_exps)
+    data_3 = read('./data/2018年心身科门诊数据.csv', cols = cols, is_filter= is_filter,
+                  filter_cols = filter_cols, filter_exps = filter_exps)
+    return pd.concat([data_1, data_2, data_3], ignore_index=False)
+
+
+def read(file:str,
+         cols:list=['PATIENT_ID', 'VISIT_DATE', 'SEX', 'DIAG_DESC', 'ILLNESS_DESC'],
+         is_filter:bool = True,
+         filter_cols:list = ['ILLNESS_DESC'],
+         filter_exps:list = [PAT]):
+    '''
+    读取csv格式的文件
+    :param file: csv文件名
+    :param cols: 要保留的字段
+    :param filter_cols: 需要过滤的字段
+    :param filter_exps: 需要过滤字段的正则表达式
+    :return: 读取的数据
+    '''
+    try:
+        data = pd.read_csv(file, encoding='utf-8')
+        for c in cols:
+            data[c].fillna('', inplace = True)
+        if is_filter:
+            for col, exp in zip(filter_cols, filter_exps):
+                data = filter(data, col, exp)
+        return data[cols]
+    except Exception as e:
+        print(e.message)
+
+
+def filter(data:pd.DataFrame, col:str, kw_exp:str):
+    '''
+    :param data: 要过滤的数据，数据类型为DataFrame
+    :param col: 要过滤的字段
+    :param kw_exp: 匹配字符的正则表达式
+    :return: 过滤后的数据
+    '''
+    return data[~data[col].str.contains(kw_exp)]
+
+
+def get_outpatient_volume(data:pd.DataFrame):
+    '''
+    统计每天的门诊量
+    :param data: 原始的门诊量访问数据
+    :return: 每天的门诊量
+    '''
+    result = data.groupby(by='VISIT_DATE', as_index=False).size()
+    return pd.DataFrame({'date':result.index,'volume':result.values})
+
+
+def merage_file(folder, outputfile):
+    '''
+    合并文件
+    :param folder:原始文件所在的文件夹
+    :param outputfile:需要输出的文件的位置
+    :return:
+    '''
+    with open(outputfile, 'w', encoding='utf-8') as newfile:
+        for f in os.listdir(folder):
+            with open(os.path.join(folder, f), 'r',encoding='utf-8') as readfile:
+                newfile.write(readfile.read())
+
+def format_print(result, title = ''):
+    '''
+    按照一定的格式输出想要查看的结果
+    :param result:
+    :return:
+    '''
+    if title:
+        print(title)
+    for i in result:
+        print("{}:{}".format(i[0], i[1]))
+    print('='*30)
+
+def result_intersection(results):
+    new_results = list()
+    for r in results:
+        new_set = set()
+        for i in r:
+            new_set.add(i[0])
+        new_results.append(new_set)
+    return set.intersection(*new_results)
+
+def compute_term_rank(analyzer, topK, poses = ['n']):
+    print('Computing TF-IDF...')
+    tf_idf_tag = analyzer.get_tf_idf_rank(poses, topK)
+    print('Computing TextRank...')
+    txtrank_tag = analyzer.get_textrank_rank(poses, topK)
+    print('Computing Frequency...')
+    freq_tag = analyzer.get_freq_rank(poses, topK)
+    return [tf_idf_tag, txtrank_tag, freq_tag]
+
+def display_term_rank_result(result):
+    format_print(result[0], 'TF-IDF results:')
+    format_print(result[1], 'TextRank results:')
+    format_print(result[2], 'Freqency results:')
+    df_tfidf = pd.DataFrame([dict(result[0])]).T
+    df_tfidf.columns = ['tfidf']
+    df_txtrank = pd.DataFrame([dict(result[1])]).T
+    df_txtrank.columns = ['textrank']
+    df_freq = pd.DataFrame([dict(result[2])]).T
+    df_freq.columns = ['freq']
+    print('top {} intersection are:{}'.format(len(result[0]), result_intersection(result)))
+
+
