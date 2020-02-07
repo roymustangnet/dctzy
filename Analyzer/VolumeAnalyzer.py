09ccf06cddb878dcd579432a5b171eccaee32460
supakito <roy.mustang.net@gmail.com>
2020-02-07 15:30:03 +0800

    为门诊量数据添加了星期属性，修改了ipynb部分代码

---
+++ Analyzer/VolumeAnalyzer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :VolumeAnalyzer.py
+# @Time      :2020/2/7 10:46
+# @Author    :supakito
+
+
+if __name__ == "__main__":
+    run_code = 0
